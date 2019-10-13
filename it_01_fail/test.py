9e439d25164fda5205965929aa640acc8d4bd036
tatoem <tatoem@ttu.ee>
2019-10-13 19:12:40 +0300

    added failing tests

--- it_01_fail/test.py
+++ it_01_fail/test.py
@@ -28,5 +28,50 @@ class Test(unittest.TestCase):
 
         self.assertFalse(result)
 
+    def test_logging_in(self):
+        """
+        Test that the registration component rejects invalid passwords.
+        """
+        system = bootstrapper.create()
+        result = system.register_user('name', 'pass')
+
+        self.assertFalse(result)
+
+    def test_invalid_site_type(self):
+        """
+        Test that site type selection rejects invalid types.
+        """
+        system = bootstrapper.create()
+        result = system.select_type_of_site('School')
+
+        self.assertFalse(result)
+
+    def test_valid_site_type(self):
+        """
+        Test that site type selection accepts valid types.
+        """
+        system = bootstrapper.create()
+        result = system.select_type_of_site('Business')
+
+        self.assertTrue(result)
+
+    def test_valid_site_name(self):
+        """
+        Test that site name selection accepts valid names.
+        """
+        system = bootstrapper.create()
+        result = system.select_name_of_site('Test')
+
+        self.assertTrue(result)
+
+    def test_invalid_site_name(self):
+        """
+        Test that site name selection accepts valid names.
+        """
+        system = bootstrapper.create()
+        result = system.select_name_of_site('Ab')
+
+        self.assertFalse(result)
+
 if __name__ == '__main__':
     unittest.main()
