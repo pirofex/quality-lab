562928c8dfe27df236fc8fec718880d5497a4e59
Lukas Feldmann <lukas.feldmann@siemens.com>
2019-10-21 17:39:16 +0200

    add calculator program for code coverage

---
+++ coverage/calculator/cli.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python2
+
+import readline
+import logging
+import calculator as calc
+
+def readline_configure():
+    readline.parse_and_bind('tab: complete')
+
+def main():
+    help_msg = \
+"This is calculator for basic math expressions.\n\
+Input correct expression and press <ENTER>.\n\
+Press 'q <Enter>' to exit."
+    readline_configure()
+
+    c = calc.Calculator()
+    
+    while True:
+        user_input = raw_input('> ')
+        if user_input == 'h':
+            print(help_msg)
+        elif user_input == 'q':
+            break
+        else: 
+            try:
+                result = c.calculate("".join(user_input))
+            except (calc.parser.ParseError, 
+                    calc.converter.ConvertError,
+                    calc.converter.ConvertError,
+                    calc.evaluator.EvaluateError,
+                    ArithmeticError,
+                    ValueError) as e:
+                print("Error: " + str(e))
+            else:
+                print("= " + str(result))
---
+++ coverage/calculator/parser.py
@@ -0,0 +1,78 @@
+import logging
+from collections import defaultdict
+import re
+import definitions
+
+class ParseError(Exception):
+    def __init__(self, eExpr, ePos):
+        self.expr = eExpr
+        self.pos = ePos
+    def __str__(self):
+        msg = 'unknown token at ' + str(self.pos) + ': \n'
+        msg += self.expr + '\n'
+        msg += ''.join([' ' for i in xrange(self.pos - 1)]) + '---'
+        return msg
+
+class Parser(object):
+    def __init__(self):
+        self.numberM = definitions.Identifier(
+            type = 'num', val = re.compile(definitions._number.regex))
+        
+        self.functionM = definitions.Identifier(type = 'func', val = re.compile( '|'.join( 
+            [re.escape(func) for func in sorted(definitions._functions, key=lambda func: len(func), reverse=True) ] )))
+
+        self.operatorM = definitions.Identifier(type = 'op', val = re.compile( '|'.join( 
+            [re.escape(op) for op in sorted(definitions._operators, key=lambda func: len(func), reverse=True)] )))
+        
+        self.lBracketM = definitions.Identifier(type = 'lb', val = re.compile('\('))
+        self.rBracketM = definitions.Identifier(type = 'rb', val = re.compile('\)'))
+
+        self.matchers = (self.numberM, self.functionM, self.operatorM, self.lBracketM, self.rBracketM)
+
+
+    def parse(self, iExpr):
+        '''
+        @iExpr -- string to parse
+        @parsedExpr -- output parsed expression
+        '''
+
+        logging.info('Parser started.')
+        logging.debug('Parser IO:')
+        logging.debug('Input: ' + iExpr)
+
+        parsedExpr = []
+        curPos = 0
+
+        while curPos < len(iExpr):
+            for matcher in self.matchers:
+                result = matcher.val.match(iExpr, curPos)
+                if result:
+                    if result.re == self.numberM.val:
+                        parsedExpr.append( \
+                            definitions.Identifier(type = matcher.type, val = float(result.group(0))))
+                    else:
+                        parsedExpr.append( \
+                            definitions.Identifier(type = matcher.type, val = result.group(0)))
+                    curPos = result.end()
+                    break
+                elif matcher == self.matchers[-1]:
+                    # can't find suitable matcher
+                    logging.debug('Output: ' + ', '.join([ str(ident.val) for ident in parsedExpr ]) + '.')
+                    raise ParseError(iExpr, curPos)
+                    
+
+
+        logging.debug('Output: ' + ', '.join([ str(ident.val) for ident in parsedExpr ]) + '.')
+
+        parseStat = defaultdict(int)
+        for ident in parsedExpr:
+            parseStat[ident.type] += 1
+
+        statMsg = 'Parser statistics: \n'
+        statMsg += '- ' + str(parseStat['op']) + ' operators,\n'
+        statMsg += '- ' + str(parseStat['func']) + ' functions,\n'
+        statMsg += '- ' + str(parseStat['lb']) + ' opening brackets,\n'
+        statMsg += '- ' + str(parseStat['rb']) + ' closing brackets.'
+        logging.info(statMsg)
+
+        return parsedExpr
